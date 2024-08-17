a4e9a832398f5be66681c8215791f4ed73010c6f
shubham <kumarshubham.maldah@gmail.com>
2024-08-17 11:11:42 +0530

    success

---
+++ java function/first.java
@@ -0,0 +1,13 @@
+public class first {
+    public static void main(String[] args) {
+        sum();
+    }
+    public static void sum()
+    {
+        int a =2;
+        int b=3;
+        int su=a+b;
+        System.out.println(su);
+    }
+}
+
